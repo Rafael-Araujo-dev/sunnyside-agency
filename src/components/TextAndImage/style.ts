a93ff5c772f7c236d5c8baba3b5755ff5535d827
Rafael Araujo <90640158+Rafael-Araujo-dev@users.noreply.github.com>
2022-04-11 18:19:00 -0300

    feat: creation of the text and image component

---
+++ src/components/TextAndImage/style.ts
@@ -0,0 +1,113 @@
+import styled from "styled-components";
+import colors from "@styles/colors.json";
+import typograph from "@styles/typograph.json";
+
+
+export const Container = styled.section`
+    display: flex;
+    flex-direction: column;
+    background-color: ${colors.neutral100};
+
+    @media (min-width: 768px) {
+        flex-direction: row;
+    }
+`
+
+interface ContentProperties { 
+    order?: number;
+}
+export const Content = styled.div<ContentProperties>`
+    padding: 15% 10%;
+    text-align: center;
+
+    @media (min-width: 768px) {
+        width: calc(50vw - 20%);
+        padding: 10% 0%;
+        margin: auto;
+        text-align: left;
+        order: ${props => props.order};
+    }
+`
+
+interface textProperties { 
+    color?: string;
+}
+
+export const Title = styled.h2<textProperties>`
+    font-family: 'Fraunces';
+    color: ${props => props.color};
+    font-size: ${typograph.SubtitleSmall.fontSize};
+    line-height: ${typograph.SubtitleSmall.lineHeight};
+    font-weight: ${typograph.SubtitleSmall.fontWeight.bolder};
+    margin-bottom: 30px;
+
+    @media (min-width: 768px) {
+        font-size: ${typograph.SubtitleLarge.fontSize};
+        line-height: ${typograph.SubtitleLarge.lineHeight};
+        font-weight: ${typograph.SubtitleLarge.fontWeight.bolder};
+        margin-bottom: 60px;
+    }
+`
+
+export const TextContainer = styled.p`
+    margin-bottom: 30px;
+    color: ${props => props.color};
+    font-size: ${typograph.bodySmall.fontSize};
+    line-height: ${typograph.bodySmall.lineHeight};
+    font-weight: ${typograph.bodySmall.fontWeight.normal};
+    
+    @media (min-width: 768px) {
+        font-size: ${typograph.bodyLarge.fontSize};
+        line-height: ${typograph.bodyLarge.lineHeight};
+        font-weight: ${typograph.bodyLarge.fontWeight.normal};
+        margin-bottom: 60px;
+    }    
+`
+
+export const Link = styled.a<textProperties>`
+    font-family: 'Fraunces';
+    text-decoration: none;
+    text-transform: uppercase;
+    color: ${colors.neutral500};
+    font-size: ${typograph.bodySmall.fontSize};
+    line-height: ${typograph.bodySmall.lineHeight};
+    font-weight: ${typograph.bodySmall.fontWeight.bolder};
+    position: relative;
+    z-index: 2;
+    &:after{
+        position: absolute;
+        background-color: ${props => props.color};
+        width: 100%;
+        height: 12px;
+        content: "";
+        padding: 0 10px;
+        border-radius: 4px;
+        bottom: 0;
+        left: -10px;
+        z-index: -1;
+    }
+    @media (min-width: 768px) {
+        font-size: ${typograph.bodyLarge.fontSize};
+        line-height: ${typograph.bodyLarge.lineHeight};
+        font-weight: ${typograph.bodyLarge.fontWeight.bolder};
+        margin-left: 10px;
+    }    
+`
+
+interface ImageContainerProps {
+    bgMobile?: string,
+    bgDesk?: string,
+}
+
+export const ImageContainer = styled.div<ImageContainerProps>`
+    height: 50vh;
+    width: 100%;
+    background-image: url(${props => props.bgMobile ? props.bgMobile : props.bgDesk});
+    background-size: cover;
+    background-position: center;
+    @media (min-width: 768px) {
+        background-image: url(${props => props.bgDesk ? props.bgDesk : props.bgMobile});
+        width: 50vw;
+        height: auto;
+    }
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,36 @@ import Head from "next/head";
 
 //Components
 import HeroMain from "@components/HeroMain";
+import TextAndImage from "@components/TextAndImage";
+
+const props = {
+  textAndImages: {
+    textAndImage1: {
+      image: {
+        desk: "./components/TextAndImages/image-transform-desk.jpg",
+        mobile: "./components/TextAndImages/image-transform-mobile.jpg",
+      },
+      title: "Transform your brand",
+      titleColor: "#23303e",
+      text: "We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.",
+      textColor: "#818498",
+      link: "#",
+      linkColor: "#FFF3C0",
+    },
+    textAndImage2: {
+      image: {
+        desk: "./components/TextAndImages/image-stand-out-desk.jpg",
+        mobile: "./components/TextAndImages/image-stand-out-mobile.jpg",
+      },
+      title: "Stand out to the right audience",
+      titleColor: "#23303e",
+      text: "Using a collaborative formula of designers, researches, photographers, videographers, and copywriters, we'll build and extend your brand in digital places.",
+      textColor: "#818498",
+      link: "#",
+      linkColor: "#F8DAD3",
+    },
+  },
+};
 
 const Home: NextPage = () => {
   return (
@@ -15,6 +45,16 @@ const Home: NextPage = () => {
       </Head>
 
       <HeroMain />
+      {Object.values(props.textAndImages)
+        .filter((item) => item)
+        .map((item, index) => {
+          return (
+            <TextAndImage
+              props={Object(item)}
+              order={index % 2 == 0 ? -1 : 0}
+            />
+          );
+        })}
     </>
   );
 };
