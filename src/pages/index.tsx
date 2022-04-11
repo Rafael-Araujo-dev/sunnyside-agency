cda4d67129966fd701f06482b835dff11fac697b
Rafael Araujo <90640158+Rafael-Araujo-dev@users.noreply.github.com>
2022-04-11 14:00:33 -0300

    fix: remove navbar do index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-//Components
-import Navbar from "@components/Navbar";
-
 const Home: NextPage = () => {
   return (
     <>
@@ -14,7 +11,6 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar />
     </>
   );
 };
